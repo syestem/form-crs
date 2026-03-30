d1ae3df8c4da7aaebac444379d4fe4e7b0d7454a
syestem <xanaxoriginal@gmail.com>
2026-03-31 02:06:45 +0700

    Update and rename config.runtime.github.example.js to config.runtime.js

--- config.runtime.github.example.js
+++
@@ -1,3 +0,0 @@
-window.FORM_RUNTIME_CONFIG = {
-  apiBaseUrl: "https://your-project-ref.supabase.co/functions/v1/form-api"
-};
---
+++ config.runtime.js
@@ -0,0 +1,3 @@
+window.FORM_RUNTIME_CONFIG = {
+  apiBaseUrl: "https://qbkkvnexmjlhtularcsz.supabase.co/functions/v1/form-api"
+};
